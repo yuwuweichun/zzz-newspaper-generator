bd3070eeff6bee435505a8836fc5e8399be4403a
SongChun <yuwuweichun522@gmail.com>
2025-04-15 04:59:20 +0800

    base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'zzz-newspaper-generator',
+  base: '/zzz-newspaper-generator/',
   plugins: [vue()],
   resolve: {
     alias: {
